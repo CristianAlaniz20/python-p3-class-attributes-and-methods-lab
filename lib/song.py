1782ffbfcade9b7b47b9810aebe84f124f49923f
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-05-02 21:35:44 -0700

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,52 @@
 class Song:
-    pass
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+    
+    count = 0
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    genres = []
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if not(genre in cls.genres):
+            cls.genres.append(genre)
+    
+    artists = []
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if not(artist in cls.artists):
+            cls.artists.append(artist)
+
+    genre_count = {}
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            for gnre in cls.genres:
+                cls.genre_count.setdefault(gnre, 1)
+        
+    artist_count = {}
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            for art in cls.artists:
+                cls.artist_count.setdefault(art, 1)
+
+
